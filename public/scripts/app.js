01c6de5a24692303da80cd72900c193cb63a8eb2
Geoff Phillips <geoffreymartinphillips@gmail.com>
2018-08-15 19:44:23 +0000

    Added renderTweets function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,85 +1,126 @@
-function createArticle() {
-  const $output = $("<article>");
-  $output.attr("class", "all-tweets")
-  $output.attr("id", "all-tweets")
-  return $output;
-}
+$(function() {
 
-function createHeader(userdata) {
-  const $output = $("<header>");
-
-  const $img = $("<img>");
-  $img.attr("src", `${userdata.avatars.small}`);
-  $img.attr("alt", `${userdata.name}`);
-
-  const $username = $(`<h3>${userdata.name}</h3>`);
-  const $handle = $(`<p>${userdata.handle}</p>`);
-
-  $output.append($img[0]);
-  $output.append($username[0]);
-  $output.append($handle[0]);
-
-  return $output;
-}
-
-function createFooter(timestamp) {
-  const $output = $("<footer>");
-  // Time is initially milliseconds since 1970. Find difference in milliseconds than convert milliseconds to days
-  const currentMilliseconds = new Date().getTime();
-  const timeDifference = currentMilliseconds - timestamp;
-  const daysAgo = Math.floor(timeDifference / 1000 / 60 / 60 / 24);
-  const $time = $(`<p>${daysAgo} days ago</p>`)
-
-  $output.append($time[0]);
-
-  const $iconLike = $("<img>");
-  $iconLike.attr("src", "images/like.png");
-  $iconLike.attr("atl", "Like this tweet");
-
-  const $iconRetweet = $("<img>");
-  $iconRetweet.attr("src", "images/retweet.png");
-  $iconRetweet.attr("atl", "Retweet this tweet");
-
-  const $iconFlag = $("<img>");
-  $iconFlag.attr("src", "images/flag.png");
-  $iconFlag.attr("atl", "Flag this tweet as inappropriate");
-
-  $output.append($iconFlag[0]);
-  $output.append($iconRetweet[0]);
-  $output.append($iconLike[0]);
-
-  return $output;
-}
-
-function createTweetElement(data) {
-  const $output = createArticle();
-  const $header = createHeader(data.user);
-  const $content = $(`<p>${data.content.text}</p>`);
-  const $footer = createFooter(data.created_at);
-
-  $output.append($header[0]);
-  $output.append($content[0]);
-  $output.append($footer[0]);
-
-  return $output;
-}
-
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": {
-      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+  const tweetData = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
     },
-    "handle": "@SirIsaac"
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
   },
-  "created_at": 1461116232227
-}
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
 
-var $tweet = createTweetElement(tweetData);
+  function createArticle() {
+    const $output = $("<article>");
+    $output.attr("class", "all-tweets")
+    $output.attr("id", "all-tweets")
+    return $output;
+  }
 
-console.log($tweet)
+  function createHeader(userdata) {
+    const $output = $("<header>");
+
+    const $img = $("<img>");
+    $img.attr("src", `${userdata.avatars.small}`);
+    $img.attr("alt", `${userdata.name}`);
+
+    const $username = $(`<h3>${userdata.name}</h3>`);
+    const $handle = $(`<p>${userdata.handle}</p>`);
+
+    $output.append($img);
+    $output.append($username);
+    $output.append($handle);
+
+    return $output;
+  }
+
+  function createFooter(timestamp) {
+    const $output = $("<footer>");
+    // Time is initially milliseconds since 1970. Find difference in milliseconds than convert milliseconds to days
+    const currentMilliseconds = new Date().getTime();
+    const timeDifference = currentMilliseconds - timestamp;
+    const daysAgo = Math.floor(timeDifference / 1000 / 60 / 60 / 24);
+    const $time = $(`<p>${daysAgo} days ago</p>`)
+
+    $output.append($time);
+
+    const $iconLike = createIcon$("like");
+    const $iconRetweet = createIcon$("retweet");
+    const $iconFlag = createIcon$("flag");
+
+    $output.append($iconLike);
+    $output.append($iconRetweet);
+    $output.append($iconFlag);
+
+    return $output;
+  }
+
+  function createIcon$(iconName) {
+    const $icon = $("<img>");
+    $icon.attr("class", "icons");
+    $icon.attr("src", "images/" + iconName + ".png");
+    $icon.attr("alt", iconName + " this tweet");
+    return $icon;
+  }
+
+
+  function createTweetElement(data) {
+    const $output = createArticle();
+    const $header = createHeader(data.user);
+    const $content = $(`<p>${data.content.text}</p>`);
+    const $footer = createFooter(data.created_at);
+
+    $output.append($header);
+    $output.append($content);
+    $output.append($footer);
+
+    return $output;
+  }
+
+  function renderTweets(tweets) {
+    tweets.forEach(tweet => {
+      const output = createTweetElement(tweet);
+      $("main.container").append(output);
+    });
+  }
+
+  renderTweets(tweetData);
+
+});
