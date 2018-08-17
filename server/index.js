ff43c663a3751ac511c4a9fdfdf96331519cc70d
Geoff Phillips <geoffreymartinphillips@gmail.com>
2018-08-17 21:05:43 +0000

    Third herkou test

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 const MongoClient = require("mongodb").MongoClient;
-const MONGODB_URI = "mongodb://", process.env.MONGODB_URI, "/tweeter";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/tweeter";
 
 MongoClient.connect(MONGODB_URI, (err, db) => {
   if (err) {
